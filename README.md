566df2c2264441e6c795f6a24e31c6e062bbb69b
zero780 <milton.garcia1998@hotmail.com>
2019-07-10 20:05:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# administrador-buyhelp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/administrador-buyhelp)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './index/main/main.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+//import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, MainComponent, HeaderComponent, FooterComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
