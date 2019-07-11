9ba123d986b8cfc0cf7ac054e7243bea49fd6c2e
zero780 <milton.garcia1998@hotmail.com>
2019-07-11 13:22:37 -0500

    adminUsuario y gestionProcesos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './index/main/main.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AdminUsuariosComponent } from './secundarias/admin-usuarios/admin-usuarios.component';
+
+import { GestionProcesosComponent } from './secundarias/gestion-procesos/gestion-procesos.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const rutas: Routes = [
+ { path: '', component: MainComponent },
+ { path: 'admin_usuarios', component: AdminUsuariosComponent },
+ { path: 'gestion_procesos', component: GestionProcesosComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MainComponent, HeaderComponent, FooterComponent ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(rutas) ],
+  declarations: [ AppComponent, MainComponent, HeaderComponent, FooterComponent, AdminUsuariosComponent, GestionProcesosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/secundarias/gestion-procesos/gestion-procesos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-procesos',
+  templateUrl: './gestion-procesos.component.html',
+  styleUrls: ['./gestion-procesos.component.css']
+})
+export class GestionProcesosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
